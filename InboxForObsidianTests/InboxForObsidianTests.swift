bb92893bdeb69ff6090cf7217f69c9d92e70ec6c
Joseph R. Jones <jrj@jrj.org>
2025-04-05 02:36:18 -0400

    Initial Commit

---
+++ InboxForObsidianTests/InboxForObsidianTests.swift
@@ -0,0 +1,16 @@
+//
+//  InboxForObsidianTests.swift
+//  InboxForObsidianTests
+//
+//  Created by Joseph R. Jones on 4/5/25.
+//
+
+import Testing
+
+struct InboxForObsidianTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
